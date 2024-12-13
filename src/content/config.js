9bf60fde6fb4186e071aadbee4252f1f9b7e7eff
Aarush Saboo <160993404+aarushsaboo@users.noreply.github.com>
2024-12-13 21:19:38 +0530

    fixing huge bug

--- src/content/config.js
+++ src/content/config.js
@@ -1,10 +1,22 @@
 export const CONFIG = {
-  ALLOWED_PAGES: ["/", "/feed/subscriptions", "/feed/trending"],
+  ALLOWED_PAGES: [
+    "/",
+    "/channel/UCEgdi0XIXXZ-qJOFPf4JSKw",
+    "/channel/UCYfdidRxbB8Qhf0Nx7ioOYw",
+    "/gaming",
+    "/channel/UC4R8DWoMoI7CAwX8_LjQHig",
+    "/feed/storefront?bp=ogUCKAU%3D",
+    "/feed/trending?bp=6gQJRkVleHBsb3Jl",
+  ],
   SELECTORS: {
-    videosAndShorts: "ytd-rich-item-renderer h3",
+    videosAndShorts: [
+      "ytd-rich-item-renderer h3",
+      "ytd-grid-video-renderer h3 a",
+      "ytm-shorts-lockup-view-model-v2 h3 a",
+      "ytd-grid-movie-renderer h3 span",
+    ],
   },
 }
-
 
 // Structure for shorts
 // ytd-rich-item-renderer (#content or .ytd-rich-item-renderer) (ytm-shorts-lockup-view-model-v2) (ytm-shorts-lockup-view-model) (.shortsLockupViewModelHostOutsideMetadata shortsLockupViewModelHostMetadataRounded image-overlay-text) (h3 with an aria-label)
--- src/content/main.js
+++ src/content/main.js
@@ -40,6 +40,7 @@ async function init() {
 // Initialize on page load
 if (document.readyState === "loading") {
   document.addEventListener("DOMContentLoaded", init)
+    console.log("This is filtering YOUTHOOB... ALERTTTTTT!")
 } else {
   init()
 }
